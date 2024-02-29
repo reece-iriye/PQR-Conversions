a1b71ebc89def7b8a9d6663c76a9d07739eb0f3a
ririye01 <ririye01@gmail.com>
2024-02-29 09:46:02 -0600

    Error handling

--- comparison.py
+++ comparison.py
@@ -1,5 +1,6 @@
 import sys
 from typing import List, Tuple, Any
+from typing_extensions import Optional
 
 
 def _is_subset(subset: List[Any], superset: List[Any]) -> bool:
@@ -20,27 +21,27 @@ def compare_files(
     protein_file_path: str,
     ligand_file_path: str,
     complex_file_path: str,
-) -> Tuple[bool, bool, int]:
+) -> Optional[Tuple[bool, bool, int]]:
     """
     Compare contents of three text files containing protein, ligand, and complex data.
-    
+
     Parameters:
         protein_file_path (str): The file path of the protein data file.
         ligand_file_path (str): The file path of the ligand data file.
         complex_file_path (str): The file path of the complex data file.
-    
+
     Returns:
         Tuple[bool, bool, int]: A tuple containing:
             - The first boolean indicates whether the protein and ligand data are subsets of the complex data.
             - The second boolean indicates whether the complex data exclusively contains lines from both protein and ligand data.
-            - The integer representing the count of lines that are not part of the subset if both comparisons are false. 
+            - The integer representing the count of lines that are not part of the subset if both comparisons are false.
     Raises:
         FileNotFoundError: If one or more files specified by the file paths do not exist.
     """
     try:
         with (
-            open(protein_file_path, 'r') as protein_file, 
-            open(ligand_file_path, 'r') as ligand_file, 
+            open(protein_file_path, 'r') as protein_file,
+            open(ligand_file_path, 'r') as ligand_file,
             open(complex_file_path, 'r') as complex_file,
         ):
             protein_lines: List[str] = protein_file.readlines()
@@ -52,24 +53,24 @@ def compare_files(
             #   while still maintaining the rest of the line
             protein_atom_lines = [line[13:] for line in protein_lines if line.startswith("ATOM")]
             ligand_atom_lines = [line[13:] for line in ligand_lines if line.startswith("ATOM")]
-            complex_atom_lines = [line[13:] for line in complex_lines if line.startswith("ATOM")]            
+            complex_atom_lines = [line[13:] for line in complex_lines if line.startswith("ATOM")]
 
             print(f"{protein_file_path} contains {len(protein_atom_lines)} atoms.")
             print(f"{ligand_file_path} contains {len(ligand_atom_lines)} atoms.")
             print(f"{complex_file_path} contains {len(complex_atom_lines)} atoms.\n")
-            
+
             # Check if protein_atom_lines and ligand_atom_lines are subsets of complex_atom_lines
             are_subsets = (
-                _is_subset(protein_atom_lines, complex_atom_lines) and 
+                _is_subset(protein_atom_lines, complex_atom_lines) and
                 _is_subset(ligand_atom_lines, complex_atom_lines)
             )
 
             # Check if complex_atom_lines contains exclusively lines from protein_atom_lines and ligand_atom_lines
             exclusively_from_both = _contains_exclusively(
-                sublist  = protein_atom_lines + ligand_atom_lines, 
+                sublist  = protein_atom_lines + ligand_atom_lines,
                 mainlist = complex_atom_lines,
             )
-            
+
             exclusively_from_protein = _contains_exclusively(
                 sublist  = protein_atom_lines,
                 mainlist = complex_atom_lines,
@@ -82,15 +83,15 @@ def compare_files(
 
             print("Protein-ligand file contains data exclusively from protein PQR file?:", str(exclusively_from_protein))
             count_different_protein_lines = len([
-                line 
-                for line in protein_atom_lines 
+                line
+                for line in protein_atom_lines
                 if line not in complex_atom_lines
             ])
             print(f"Number of lines different in these two files: {count_different_protein_lines}", end="\n\n")
 
             print("Protein-ligand file contains data exclusively from ligand PQR file?:", str(exclusively_from_ligand))
             count_different_ligand_lines = len([
-                line 
+                line
                 for line in ligand_atom_lines
                 if line not in complex_atom_lines
             ])
@@ -98,14 +99,14 @@ def compare_files(
 
             if not are_subsets and not exclusively_from_both:
                 count_not_in_subset = len([
-                    line 
-                    for line in protein_atom_lines + ligand_atom_lines  
-                    if line not in complex_atom_lines 
+                    line
+                    for line in protein_atom_lines + ligand_atom_lines
+                    if line not in complex_atom_lines
                 ])
                 return are_subsets, exclusively_from_both, count_not_in_subset
             else:
                 return are_subsets, exclusively_from_both, 0
-            
+
     except FileNotFoundError:
         print("One or more files does not exist.")
 
@@ -116,13 +117,16 @@ if __name__ == "__main__":
     COMPLEX_FILE_PATH: str = sys.argv[3]
 
     print("\nOUTPUT:\n")
-    
-    are_subsets, exclusively_from_both, count_not_in_subset = compare_files(
-        protein_file_path = PROTEIN_FILE_PATH, 
-        ligand_file_path  = LIGAND_FILE_PATH, 
+
+    result = compare_files(
+        protein_file_path = PROTEIN_FILE_PATH,
+        ligand_file_path  = LIGAND_FILE_PATH,
         complex_file_path = COMPLEX_FILE_PATH,
     )
-    print("Are Subsets?:", str(are_subsets))
-    print("Are Exclusively from both?:", str(exclusively_from_both))
-    print("Total not in subset: ", str(count_not_in_subset))
-    
+    if result is not None:
+        are_subsets, exclusively_from_both, count_not_in_subset = result
+        print("Are Subsets?:", str(are_subsets))
+        print("Are Exclusively from both?:", str(exclusively_from_both))
+        print("Total not in subset: ", str(count_not_in_subset))
+    else:
+        print("nothing returned")
